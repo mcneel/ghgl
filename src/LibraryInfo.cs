8814b7104280004af60078bd37ca91d52ea1ab5b
Steve Baer <steve@mcneel.com>
2019-03-31 14:34:13 -0700

    roll to 0.3.0 version

--- src/LibraryInfo.cs
+++ src/LibraryInfo.cs
@@ -20,6 +20,6 @@ namespace ghgl
 
         public override string AuthorContact => "https://github.com/mcneel/ghgl";
 
-        public override string AssemblyVersion => "0.2.0";
+        public override string AssemblyVersion => "0.3.0";
     }
 }
