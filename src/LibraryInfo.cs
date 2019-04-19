27cc0d6edc222585172d1ff8b9b03480585db249
Steve Baer <steve@mcneel.com>
2019-04-18 21:10:44 -0700

    Add support for mesh vertex colors

--- src/LibraryInfo.cs
+++ src/LibraryInfo.cs
@@ -20,6 +20,6 @@ namespace ghgl
 
         public override string AuthorContact => "https://github.com/mcneel/ghgl";
 
-        public override string AssemblyVersion => "0.3.4";
+        public override string AssemblyVersion => "0.3.5";
     }
 }
