cd7d1ffe571cd02dc4d63db1ea7327cb273036ad
eirannejad <eirannejad@gmail.com>
2026-04-07 15:48:05 -0700

    Upped version and copyright message

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robert McNeel & Associates")]
 [assembly: AssemblyProduct("ghgl")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("48d4ff98-7772-4b59-88e2-7176f2e177cb")]
-[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyVersion("9.0.0.0")]
 
 
 namespace ghgl
